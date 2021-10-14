8cfc585bf0bc58f9ee929089e71d9f1892b86cbe
Alex Capstick <56758281+alexcapstick@users.noreply.github.com>
2021-10-14 18:51:12 +0100

    Update __init__.py
    Adding check to see if confidential folder exists.

--- minder_utils/configurations/__init__.py
+++ minder_utils/configurations/__init__.py
@@ -1,5 +1,11 @@
 import os
 
+
+p = os.path.join(os.path.dirname(__file__), 'confidential/')
+if not p.exists():
+	os.mkdir(p)
+
+
 data_path = os.path.join(os.path.dirname(__file__), 'confidential/data_path.txt')
 token_path = os.path.join(os.path.dirname(__file__), 'confidential/token_real.json')
 dates_path = os.path.join(os.path.dirname(__file__), 'confidential/dates.json')
