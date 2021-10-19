b537560b5bd7909d3373266edbf273db067bfee9
mozzielol <mozzielol@hotmail.com>
2021-10-19 15:25:58 +0100

    Create __init__.py

---
+++ minder_utils/models/feature_selectors/__init__.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+
+class Feature_selector_template(ABC):
+    def __init__(self, model):
+        self.name = self.methods[model]
+        self.model = getattr(self, model)
+
+    @property
+    @abstractmethod
+    def methods(self):
+        pass
+
+    def reset_model(self, model):
+        self.name = self.methods[model]
+        self.model = getattr(self, model)()
+
+    def get_info(self, verbose=False):
+        if verbose:
+            print('Available methods:')
+            for idx, key in enumerate(self.methods):
+                print(str(idx).ljust(10, ' '), key.ljust(10, ' '), self.methods[key].ljust(10, ' '))
+        return self.methods
+
+    @abstractmethod
+    def fit(self, X, y):
+        pass
+
+    @abstractmethod
+    def transform(self, X):
+        pass
