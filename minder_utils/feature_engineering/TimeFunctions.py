644bdf131ad102b9485a64ce254d50ff935bee77
Alex Capstick <56758281+alexcapstick@users.noreply.github.com>
2021-11-15 15:55:30 +0000

    Update TimeFunctions.py
    Added functions for calculating delta reverse percentage.

--- minder_utils/feature_engineering/TimeFunctions.py
+++ minder_utils/feature_engineering/TimeFunctions.py
@@ -3,7 +3,36 @@ import pandas as pd
 from .DensityFunctions import BaseDensityCalc
 
 
-def single_location_delta(input_df, single_location, columns={'time': 'time', 'location': 'location'}, recall_value=5):
+def raw_delta_calc(times):
+    '''
+    Given an array of times, this function calculates the deltas between them.
+
+    Arguments
+    ---------
+    
+    - times: array:
+        This is an array of times that will be used to calculate the deltas.
+
+    Returns
+    ---------
+
+    - out: array:
+        This is an array of deltas.
+
+    '''
+
+    out = (times[1:] - times[:-1])*1e-9
+
+    out = out.astype(float)
+
+
+    return out
+
+
+
+def single_location_delta(input_df, single_location, 
+                            columns={'time': 'time', 'location': 'location'}, recall_value=5, 
+                            return_as_list = False):
     '''
     This function takes the ```input_df``` and calculates the raw time delta between the single_location location time
     and the time of the ```recall_value``` number of locations immediately before the single_location.
@@ -30,6 +59,11 @@ def single_location_delta(input_df, single_location, columns={'time': 'time', 'l
 
     - recall_value: integer:
         This is the number of previous locations to the single_location trigger
+
+    - return_as_list: bool:
+        This option allows the user to return a list of the dates and data if ```True```. This is 
+        used internally by other functions.
+
     
     Returns
     ---------
@@ -73,12 +107,25 @@ def single_location_delta(input_df, single_location, columns={'time': 'time', 'l
     # out dictionary
     out = {}
 
-    # creating the output dictionary
-    for date in single_location_dates.unique():
-        # saving the delta values for this date to the dictionary
-        out[pd.to_datetime(date)] = recall_delta[single_location_dates.values == date]
 
-    return out
+    if return_as_list:
+        date_list = []
+        data_list = []
+        for nd, date in enumerate(single_location_dates.unique()):
+            date_list.append(date)
+            data_to_add = recall_delta[single_location_dates.values == date].astype(float)
+            data_list.append(data_to_add)
+        
+        return pd.to_datetime(date_list), data_list
+
+
+    else:
+        # creating the output dictionary
+        for date in single_location_dates.unique():
+            # saving the delta values for this date to the dictionary
+            out[pd.to_datetime(date)] = recall_delta[single_location_dates.values == date].astype(float)
+
+        return out
 
 
 class TimeDeltaDensity(BaseDensityCalc):
@@ -94,6 +141,115 @@ class TimeDeltaDensity(BaseDensityCalc):
                                  sample=sample, sample_size=sample_size, seed=seed, verbose=verbose)
 
         return
+
+
+
+
+def rp_single_location_delta(input_df, single_location, baseline_length_days = 7, baseline_offset_days = 0,
+                             columns={'time': 'time', 'location': 'location'}, recall_value=5):
+    '''
+    This function takes the ```input_df``` and calculates the reverse percentage time delta between the ```single_location``` location time
+    and the time of the ```recall_value``` number of locations immediately before the ```single_location```. The baseline
+    for the rever percentage calculation is defined by ```baseline_length_days``` and ```baseline_offset_days```. 
+
+    For example:
+    With ```baseline_length_days = 7``` and ```baseline_offset_days = 1```, the rp deltas on the day
+    ```pd.Timestamp('2021-06-29')``` are calculated using the deltas from 
+    ```pd.Timestamp('2021-06-21 00:00:00')``` to ```pd.Timestamp('2021-06-28 00:00:00')```.
+
+    This does not separate on subject. Please pass data from a single subject into this function.
+
+    Arguments
+    ---------
+
+    - input_df: pandas dataframe:
+        This is a dataframe that contains columns relating to the subject, time and location of sensor trigger.
+
+    - single_location: string:
+        This is the location value that you wish to calculate the delta to.
+    
+    - baseline_length_days: integer:
+        This is the length of the baseline in days that will be used. This value is used when finding
+        the ```baseline_length_days``` complete days of ```single_location``` data to use as a baseline.
+    
+    - baseline_offset_days: integer:
+        This is the offset to the baseline period. ```0``` corresponds to a time period ending the morning of the
+        current date being calculated on.
+    
+    - columns: dictionary:
+        This is the dictionary with the column names in ```input_df``` for each of the values of data that we need 
+        in our calculations.
+        This dictionary should be of the form:
+        ```
+        {'time':      column containing the times of sensor triggers,
+         'location':  column containing the locations of the sensor triggers}
+         ```
+
+    - recall_value: integer:
+        This is the number of previous locations to the single_location trigger
+    
+    
+    Returns
+    ---------
+
+    - out: dictionary:
+        This has the Timestamps of the dates as keys (for example: Timestamp('2021-05-05 00:00:00')) and the 
+        arrays of deltas as values. The arrays of deltas are of shape ```(Nt, recall_value)``` where Nt is the 
+        number of visits to ```single_location``` on a given day.
+
+    '''
+
+
+    # column names
+    time_column = columns['time']
+    location_column = columns['location']
+
+    out = {}
+
+    # format the incoming data to ensure assumptions about structure are met
+    input_df[time_column] = pd.to_datetime(input_df[time_column], utc=True)
+    input_df = input_df.sort_values(time_column)
+
+    # getting the single location raw delta
+    date_list, data_list = single_location_delta(input_df, single_location, columns, recall_value, return_as_list=True)
+
+    # for each date
+    for nd, date in enumerate(date_list):
+        date = pd.to_datetime(date)
+
+
+        '''
+        if len(baseline_offset)>0:
+            baseline_start_tp = pd.to_datetime(date - pd.Timedelta(**baseline_length) - pd.Timedelta(**baseline_offset))
+            baseline_end_tp = pd.to_datetime(date - pd.Timedelta(**baseline_offset))
+        else:
+            baseline_start_tp = pd.to_datetime(date - pd.Timedelta(**baseline_length))
+            baseline_end_tp = pd.to_datetime(date)
+        '''
+
+        
+
+        index_baseline_end = np.where(date_list <= date)[0][-1]
+        
+        index_baseline_end = index_baseline_end - baseline_offset_days
+        index_baseline_start = index_baseline_end - baseline_length_days
+
+        if index_baseline_start < 0:
+            out[date] = -1*np.ones_like(data_list[nd])
+            continue
+
+        
+        baseline_delta = np.vstack([data_list[index] for index in range(index_baseline_start, index_baseline_end)])
+
+        
+        td = TimeDeltaDensity(save_baseline_array=True, sample=True, sample_size=10000,
+                                seed=None, verbose=False)
+
+        td.fit(baseline_delta)
+        out[date] = td.transform(data_list[nd])
+        
+    return out
+
 
 
 def datetime_to_clock(times):
